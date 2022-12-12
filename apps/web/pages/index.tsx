80381d78c2bf22f8edb40250885bafc3ea93b6ab
sameerul97 <sameerul97@gmail.com>
2022-12-12 12:53:58 +0000

    Add strapi

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,28 @@
-import { Button } from "ui";
+import { useEffect, useState } from 'react';
+import { Button } from 'ui';
 
 export default function Web() {
+  const [user, setUser] = useState(null);
+
+  const fetchUser = async () => {
+    const response = await fetch('http://localhost:1337/api/users');
+    const data = await response.json();
+
+    setUser(data);
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  if (!user) {
+    return <p>Loading</p>;
+  }
+
   return (
     <div>
-      <h1>Web</h1>
+      <h1>Web www</h1>
+      <p>{JSON.stringify(user)}</p>
       <Button>Hello World</Button>
     </div>
   );
