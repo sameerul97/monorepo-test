8b1d121dbc2526952245a571b2c2dd7ce95adcdf
sameerul97 <sameerul97@gmail.com>
2022-12-21 12:09:25 +0000

    update index page

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}> Next.js Github Page!</h1>
+        <h1 className={styles.title}> test Next.js Github Page!</h1>
 
         <div className={styles.grid}>
           <Link passHref href="/project">
