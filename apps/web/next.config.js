a6f40de97cd6aa1afa2a3b6d27631f80fbe0237b
sameerul97 <sameerul97@gmail.com>
2023-03-03 18:50:09 +0000

    Update base repo name in next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const isProd = process.env.NODE_ENV === 'production'
-const repoName = '/monorepo-test'
+const repoName = '/monorepo-test/next'
 
 const nextConfig = {
   reactStrictMode: true,
